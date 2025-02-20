d983f6805feee122ad80008131a846cfd83fd24c
Daniel Villafranqui <danedo20472@gmail.com>
2025-02-19 21:16:48 -0500

    Minor Changes

--- src/app/Services/crud.service.spec.ts
+++ src/app/Services/crud.service.spec.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { TestBed } from '@angular/core/testing';
 
 import { CrudService } from './crud.service';
@@ -15,21 +14,3 @@ describe('CrudService', () => {
     expect(service).toBeTruthy();
   });
 });
-=======
-import { TestBed } from '@angular/core/testing';
-
-import { CrudService } from './crud.service';
-
-describe('CrudService', () => {
-  let service: CrudService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CrudService);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
->>>>>>> f1a955c72d5dc774445b0d6d04e05f46143e61d0
