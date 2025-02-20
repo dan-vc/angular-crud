be5c87ff9767a6a7f551c91e817c215323937a3b
Daniel Villafranqui <danedo20472@gmail.com>
2025-02-19 21:45:18 -0500

    Add Database Archive

---
+++ db/angular-crud.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 20, 2025 at 03:44 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `angular-crud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personas`
+--
+
+CREATE TABLE `personas` (
+  `id` int(11) NOT NULL,
+  `nombres` varchar(255) NOT NULL,
+  `apellidos` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `personas`
+--
+
+INSERT INTO `personas` (`id`, `nombres`, `apellidos`) VALUES
+(1, 'Lucas', 'Fernández'),
+(2, 'Sofía ', 'Ramírez'),
+(3, 'Mateo', 'González'),
+(4, 'Valentina ', 'Herrera'),
+(5, 'Diego ', 'Márquez');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `personas`
+--
+ALTER TABLE `personas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `personas`
+--
+ALTER TABLE `personas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- src/app/Services/crud.service.ts
+++ src/app/Services/crud.service.ts
@@ -11,7 +11,7 @@ export class CrudService {
   constructor(private http: HttpClient) { }
 
   // Definimos una url base
-  url = 'http://localhost/projects/Senati/Angular/S5%20-%20Crud/backend'
+  url = 'http://localhost/angular/angular-crud-main/backend';
 
   // Metodo getPersona
   getPersonas() {
